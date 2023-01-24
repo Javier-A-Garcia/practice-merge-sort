c043b9b28098e5fb065029fff3b610fa11ecf3f1
Javier Garcia <javiergarcia7991@gmail.com>
2023-01-23 23:29:44 -0800

    Passing tests

--- merge-sort.js
+++ merge-sort.js
@@ -3,15 +3,22 @@
 function mergeSort(arr) {
 
   // Check if the input is length 1 or less
+  if (arr.length <= 1){
     // If so, it's already sorted: return
+    return arr;
+  }
 
   // Divide the array in half
+  let halfwayPoint = Math.floor(arr.length/2);
+  let left = arr.slice(0, halfwayPoint);
+  let right = arr.slice(halfwayPoint);
 
   // Recursively sort the left half
+  left  = mergeSort(left);
   // Recursively sort the right half
-
+  right = mergeSort(right); 
   // Merge the halves together and return
-
+  return merge(left, right);
 }
 
 
@@ -19,16 +26,40 @@ function mergeSort(arr) {
 function merge(arrA, arrB) {
 
   // Create an empty return array
+  let merged = [];
 
   // Point to the first value of each array
+  let aPointer = 0;
+  let bPointer = 0;
+
   // While there are still values in each array...
+  while (aPointer < arrA.length && bPointer < arrB.length) {
+    
     // Compare the first values of each array
-    // Add the smaller value to the return array
-    // Move the pointer to the next value in that array
+    if (arrA[aPointer] < arrB[bPointer]) {
+      // Add the smaller value to the return array
+      merged.push(arrA[aPointer]);
+      // Move the pointer to the next value in that array
+      aPointer++;
+    } else{
+      // Add the smaller value to the return array
+      merged.push(arrB[bPointer]);
+      // Move the pointer to the next value in that array
+      bPointer++;
+    }
+  }
 
+  // Accounts for cases where only 1 array has values left
+  while (aPointer < arrA.length) {
+    merged.push(arrA[aPointer]);
+    aPointer++;
+  }
+  while (bPointer < arrB.length) {
+    merged.push(arrB[bPointer]);
+    bPointer++;
+  }
   // Return the return array
-
+  return merged;
 }
 
 module.exports = [merge, mergeSort];
-
